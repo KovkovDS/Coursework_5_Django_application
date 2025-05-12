bb1cc6414226f881df46c5e8bc0469963c8c29b4
Kovkov-D-S <158774590+Kovkov-D-S@users.noreply.github.com>
2025-05-12 18:26:01 +0300

    fix code3

--- users/management/commands/add_groups.py
+++ users/management/commands/add_groups.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
+from django.contrib.auth.models import Group
 
 
 class Command(BaseCommand):
     help = 'Load test data for groups from fixture'
 
     def handle(self, *args, **kwargs):
+        Group.objects.all().delete()
 
         call_command('loaddata', 'groups_fixture.json')
         self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
